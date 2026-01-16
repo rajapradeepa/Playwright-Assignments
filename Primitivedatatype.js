22a089291a741f1ca1496f8a0f99c62bd9ae6345
rajapradeepa <47706681+rajapradeepa@users.noreply.github.com>
2026-01-16 13:19:20 +0530

    Add files via upload

---
+++ Primitivedatatype.js
@@ -0,0 +1,10 @@
+let firstName = "Raja"
+let companyName = "Testleaf"
+let mobileNumber = 9655308780
+let isAutomation = true
+let Playwright
+console.log("Datatype of", firstName , "is", typeof(firstName));
+console.log("Datatype of", companyName , "is", typeof(companyName));
+console.log("Datatype of", mobileNumber , "is", typeof(mobileNumber));
+console.log("Datatype of", isAutomation , "is", typeof(isAutomation));
+console.log("Datatype of", Playwright , "is", typeof(Playwright));
